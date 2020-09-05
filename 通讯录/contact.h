242ac84e10d42a4e3b7176dffa14fede84a6bd10
bijian0530 <1076820693@qq.com>
2020-09-06 00:22:40 +0800

    测试

---
+++ 通讯录/contact.h
@@ -0,0 +1,42 @@
+#pragma once
+#include<stdio.h>
+#include<string.h>
+
+#define MAX 1000
+#define MAX_NAME 20
+#define MAX_SEX 5
+#define MAX_TELE 12
+#define MAX_ADDR 30
+
+struct PeoInfo
+{
+	char name[MAX_NAME];
+	int age;
+	char sex[MAX_SEX];
+	char tele[MAX_TELE];
+	char addr[MAX_ADDR];
+};
+
+//通讯录类型
+struct Contact
+{
+	struct PeoInfo data[MAX];
+	int size;
+};
+
+enum Option
+{
+	EXIT,
+	ADD,
+	DEL,
+	SEARCH,
+	MODIFY,
+	SHOW
+};
+//申明函数
+void InitContact(struct Contact* ps);
+void AddContact(struct Contact* ps);
+void ShowContact(const struct Contact* ps);
+void DelContact(struct Contact* ps);
+void SearchContact(const struct Contact* ps);
+void ModifyContact(struct Contact* ps);
